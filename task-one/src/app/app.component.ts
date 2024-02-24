6b6a3b0883afb1675a67a9413e26f29bcc3470e9
Zavodny Petr <petr.zavodny@sensearena.com>
2024-02-24 07:41:36 +0100

    up to lesson 26

--- task-one/src/app/app.component.ts
+++ task-one/src/app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrl: './app.component.css,'
-  // styleUrls: ['./app.component.css'],
-  styles: [`
-    h3 { color: dodgerblue; }
-  `]
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  title = 'task-one';
 }

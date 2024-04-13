a409c00bb661727ada9919037823e4e6a8d663d6
Zavodny Petr <petr.zavodny@sensearena.com>
2024-04-13 17:56:46 +0200

    up to lesson 147

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,31 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute,
+              private router : Router) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const serverId = +this.route.snapshot.params['id'];
+    this.setServer(serverId);
+
+    this.route.params.subscribe(
+      (params) => {
+        this.setServer(+params['id']);
+      }
+    );
   }
 
+  private setServer(id: number) {
+    if (!isNaN(id) && isFinite(id)) {
+      this.server = this.serversService.getServer(id);
+    } else {
+      console.error('Invalid or absent server ID');
+      // Here, you might want to redirect the user or show an error message
+    }
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
+  }
 }

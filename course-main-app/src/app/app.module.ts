40970842bc4d0ae8b99f94eac8365b6bebddcdc0
Zavodny Petr <petr.zavodny@sensearena.com>
2024-05-04 07:20:00 +0200

    Up to lesson 236.

--- course-main-app/src/app/app.module.ts
+++ course-main-app/src/app/app.module.ts
@@ -9,7 +9,7 @@ import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import {RecipesComponent} from './recipes/recipes.component';
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import { ErrorPageComponent } from './common/error-page/error-page.component';
@@ -35,6 +35,7 @@ import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.compon
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
   ],
   providers: [ShoppingListService],
--- course-main-app/src/app/recipes/recipes-edit/recipes-edit.component.ts
+++ course-main-app/src/app/recipes/recipes-edit/recipes-edit.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {RecipeService} from "../recipe.service";
+import {Recipe} from "../../models/recipe";
 
 @Component({
   selector: 'app-recipes-edit',
@@ -8,9 +11,10 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class RecipesEditComponent implements OnInit {
   id: number;
-  editMode = false;
+  editMode: boolean = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {
   }
 
   ngOnInit(): void {
@@ -18,7 +22,56 @@ export class RecipesEditComponent implements OnInit {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
         // console.log(this.editMode);
+        this.initForm();
       }
+    )
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe: Recipe = this.recipeService.getRecipe(this.id);
+      recipeName = this.editMode ? recipe.name : '';
+      recipeImagePath = this.editMode ? recipe.imagePath : '';
+      recipeDescription = this.editMode ? recipe.description : '';
+
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(new FormGroup({
+            'name': new FormControl(ingredient.name),
+            'amount': new FormControl(ingredient.amount)
+          }));
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName, [Validators.required]),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients,
+    });
+  }
+
+
+  get controls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(''),
+        'amount': new FormControl('')
+      })
     )
   }
 }

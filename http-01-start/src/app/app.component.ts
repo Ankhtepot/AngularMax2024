e974a71bf6d1d2fc30cf7c12ef6a1fb20a9b4fce
Zavodny Petr <petr.zavodny@sensearena.com>
2024-05-08 09:16:26 +0200

    up to lesson 271

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,41 +1,59 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {environment} from "../environments/environment";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Post} from "./models/Post";
+import {PostsService} from "./posts.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loadedPosts: any[] = [];
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+  private errorSubscription: Subscription;
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private postsService: PostsService,
+  ) {
   }
 
   ngOnInit() {
+    this.errorSubscription = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
     this.onFetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    this.http.post(environment.postsUrl, postData)
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+  ngOnDestroy(): void {
+    this.errorSubscription.unsubscribe();
+  }
+
+  onCreatePost(postData: Post) {
+    this.postsService.createAndStorePost(postData);
+    this.loadedPosts.push(postData);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.http.get(environment.postsUrl)
-      .subscribe(posts => {
-        console.log(posts);
-        // this.loadedPosts = posts;
-      });
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error => {
+      this.isFetching = false;
+      this.error = error.message;
+      console.log(error);
+    });
   }
 
   onClearPosts() {
-    // Send Http request
-
+    this.postsService.deletePosts().subscribe(() => {
+      console.log('Posts deleted');
+      this.loadedPosts = [];
+    },error => {
+      this.error = error.message;
+    });
   }
 }
---
+++ http-01-start/src/app/posts.service.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../environments/environment";
+import {Post} from "./models/Post";
+import {HttpClient} from "@angular/common/http";
+import {catchError, map} from "rxjs/operators";
+import {Subject, throwError} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+  error = new Subject<string>();
+
+  constructor(private http: HttpClient) {
+  }
+
+  createAndStorePost(postData: Post) {
+    return this.http.post<{ name: string }>(environment.postsUrl, postData)
+      .subscribe(responseData => {
+        console.log(responseData);
+      }, error => {
+        this.error.next(error.message);
+      });
+  }
+
+  fetchPosts() {
+    return this.http.get<{ [key: string]: Post }>(environment.postsUrl)
+      .pipe(
+        map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              const post = responseData[key];
+              postsArray.push(new Post(post.title, post.content, key));
+            }
+          }
+          return postsArray;
+        }),
+        catchError(error => {
+          // Send to analytics
+          return throwError(error);
+        })
+      );
+  }
+
+  deletePosts() {
+    return this.http.delete(environment.postsUrl);
+  }
+}
